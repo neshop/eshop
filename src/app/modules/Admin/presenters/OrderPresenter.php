6b93ed5444342ef6c43a0547d8951082fea33970
Josef Drabek <rydercz@gmail.com>
2016-06-26 18:10:01 +0200

    objednavky

---
+++ src/app/modules/Admin/presenters/OrderPresenter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * (c) 2016 - Josef Drabek <rydercz@gmail.com>
+ */
+
+namespace App\AdminModule\Presenters;
+
+use App\Presenters\SecuredPresenter;
+
+class OrderPresenter extends SecuredPresenter
+{
+
+}
