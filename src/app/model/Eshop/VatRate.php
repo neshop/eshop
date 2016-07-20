4f44de1d17320f198d9e85ca266af652869389d1
Josef Drabek <rydercz@gmail.com>
2016-07-20 21:58:03 +0200

    entita prezentujici sazbu dph

---
+++ src/app/model/Eshop/VatRate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Model\Eshop;
+
+use Doctrine\ORM\Mapping as ORM;
+use Kdyby\Doctrine\Entities\Attributes\UniversallyUniqueIdentifier;
+use Nette\SmartObject;
+
+/**
+ * Class VatRate
+ * @package App\Model\Eshop
+ * @ORM\Entity
+ * @ORM\Table(name="vat_rate")
+ */
+class VatRate
+{
+    use SmartObject;
+    use UniversallyUniqueIdentifier;
+
+    /**
+     * @var string
+     * @ORM\Column(name="name",type="string",length=255,nullable=false)
+     */
+    private $name;
+
+    /**
+     * @var float
+     * @ORM\Column(type="float",nullable=false)
+     */
+    private $rate;
+
+}
