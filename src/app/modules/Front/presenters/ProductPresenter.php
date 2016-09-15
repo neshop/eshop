4a7b1d5cd0d67ada30e435bf7f56a6589705cf52
Josef Drabek <rydercz@gmail.com>
2016-09-16 00:12:48 +0200

    vkladani produktu z detailu do kosiku

--- src/app/modules/Front/presenters/ProductPresenter.php
+++ src/app/modules/Front/presenters/ProductPresenter.php
@@ -2,13 +2,18 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\Model\Eshop\Cart;
 use App\Model\Products\Product;
 use Doctrine\ORM\EntityManager;
+use Nette\Application\UI\Form;
 
 class ProductPresenter extends FrontPresenter
 {
     /** @var EntityManager @inject */
     public $entityManager;
+
+    /** @var Cart @inject */
+    public $cart;
 
     /** @var Product */
     private $product;
@@ -23,5 +28,24 @@ class ProductPresenter extends FrontPresenter
     {
         $template = $this->getTemplate();
         $template->product = $this->product;
+    }
+
+    public function createComponentAddToCartForm()
+    {
+        $form = new Form();
+        $form->addInteger('quantity', 'Počet')
+            ->setType('number')
+            ->setDefaultValue(1)
+            ->addRule(\Nette\Forms\Form::RANGE, 'Vyberte počet kusů', [1, 100]);
+        $form->addSubmit('submit', 'Do košíku');
+        $form->onSuccess[] = [$this, 'processAddToCartForm'];
+        return $form;
+    }
+
+    public function processAddToCartForm($form, $values)
+    {
+        $this->cart->add($this->product, $values->quantity);
+        $this->flashMessage('Produkt byl úspěšně vložen do košíku', 'success');
+        $this->redirect('this');
     }
 }
